ca55cfeb205a283e17e06d7dd770856242c4a797
Tanner Vasica <tmanvas06@hotmail.com>
2022-04-08 12:49:28 -0600

    updated

--- profile.js
+++ profile.js
@@ -20,4 +20,20 @@ const ritual = event => {
 	alert(`The Summoning of MOLAG BAL`)
 }
 
+const imageChange = () =>{
+    let image = document.getElementById(`meTheDuck`)
+    image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhFOTNe-WeSmUcf9-58R8d68vyfoCUbciwQ&usqp=CAU'
+    // let normalP = document.getElementById(`normal`)
+    // let displaySetting = normalP.style.display;
+    // if (displaySetting == `block`){
+    //     normalP.style.display == 'none'
+    
+    // }
+}
+
+
+
+favRitual.addEventListener(`click`, imageChange)
+
+
 favRitual.addEventListener(`click`, ritual)
